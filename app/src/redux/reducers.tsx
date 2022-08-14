949bbe2dcbe3b48b3cb280a21748fc2b5e3df8d9
shamalka <shamalkanavod95@gmail.com>
2022-08-14 23:45:40 +0530

    Implement token handling logic

--- app/src/redux/reducers.tsx
+++ app/src/redux/reducers.tsx
@@ -1,13 +1,16 @@
-import { SET_NEWS_DATA } from "./actions";
+import { SET_ACCESS_TOKEN, SET_NEWS_DATA } from "./actions";
 
 const initialState = {
-    newsData: []
+    newsData: [],
+    accessToken: ""
 }
 
 function newsReducer(state = initialState, action: any) {
     switch (action.type) {
         case SET_NEWS_DATA:
             return { ...state, newsData: action.payload }
+        case SET_ACCESS_TOKEN:
+            return { ...state, accessToken: action.payload }
         default:
             return state;
     }
--- app/src/redux/store.tsx
+++ app/src/redux/store.tsx
@@ -5,8 +5,5 @@ import newsReducer from './reducers';
 import Reactotron from './../../Reactotron-config'
 
 const rootReducer = combineReducers({ newsReducer });
-const reactotronEnhancer = Reactotron.createEnhancer && Reactotron.createEnhancer();
 
-const middleware = [thunk];
-reactotronEnhancer ? compose(applyMiddleware(...middleware), reactotronEnhancer) : applyMiddleware(...middleware)
-export const Store = createStore(rootReducer, reactotronEnhancer );
+export const Store = createStore(rootReducer, applyMiddleware(thunk)); 
