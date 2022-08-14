566b4d892be0cf97c5455c62b959b39045b8ed34
shamalka <shamalkanavod95@gmail.com>
2022-08-14 13:27:48 +0530

    Add news table

--- admin_panel/src/scenes/News/index.js
+++ admin_panel/src/scenes/News/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ContentHeader from "../../components/ContentHeader";
+import NewsTable from "./components/NewsTable";
 
 const News = () => {
     return (
         <div>
             <ContentHeader title="News" breadcrumbArray={["News"]}/>
+            <NewsTable/>
         </div>
     );
 };
